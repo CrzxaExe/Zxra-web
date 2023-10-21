fc8fd2bf755c2f5c30c978fff122240d33f5bcd0
CrzxaExe <craserchannel@outlook.co.id>
2023-10-21 20:32:56 +0700

    Upload

---
+++ js/index.js
@@ -0,0 +1,17 @@
+const navbar = document.getElementById("nav")
+const zxra = navbar.querySelector("#zxra")
+
+window.addEventListener("load", () => {
+  zxra.addEventListener("click", () => {
+    toggleNav()
+  })
+  
+  document.getElementById("home").addEventListener("click", () => window.location.href="index.html")
+  document.getElementById("zxrarpg").addEventListener("click", () => window.location.href="rpg.html")
+  document.getElementById("zxrawtft").addEventListener("click", () => window.location.href="wtft.html")
+})
+
+// Functions
+function toggleNav() {
+  navbar.classList.toggle("open")
+}
